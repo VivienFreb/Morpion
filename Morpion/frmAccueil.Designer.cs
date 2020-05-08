b484d0b47a182be040e499fb9237646f91d5c8e5
VivienFreb <vivien.frebourg@outlook.com>
2020-05-08 16:36:50 +0200

    Première version Morpion

---
+++ Morpion/frmAccueil.Designer.cs
@@ -0,0 +1,275 @@
+﻿namespace Morpion
+{
+    partial class frmAccueil
+    {
+        /// <summary>
+        /// Variable nécessaire au concepteur.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Nettoyage des ressources utilisées.
+        /// </summary>
+        /// <param name="disposing">true si les ressources managées doivent être supprimées ; sinon, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Code généré par le Concepteur Windows Form
+
+        /// <summary>
+        /// Méthode requise pour la prise en charge du concepteur - ne modifiez pas
+        /// le contenu de cette méthode avec l'éditeur de code.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmAccueil));
+            this.cmdButton1 = new System.Windows.Forms.Button();
+            this.cmdButton2 = new System.Windows.Forms.Button();
+            this.cmdButton4 = new System.Windows.Forms.Button();
+            this.cmdButton5 = new System.Windows.Forms.Button();
+            this.cmdButton6 = new System.Windows.Forms.Button();
+            this.cmdButton7 = new System.Windows.Forms.Button();
+            this.cmdButton8 = new System.Windows.Forms.Button();
+            this.cmdButton9 = new System.Windows.Forms.Button();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.cmdNewgame = new System.Windows.Forms.Button();
+            this.cmdQuit = new System.Windows.Forms.Button();
+            this.cmdButton3 = new System.Windows.Forms.Button();
+            this.cmdWin = new System.Windows.Forms.PictureBox();
+            this.affPlayer1 = new System.Windows.Forms.PictureBox();
+            this.affPlayer2 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.cmdWin)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.affPlayer1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.affPlayer2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // cmdButton1
+            // 
+            this.cmdButton1.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.cmdButton1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.cmdButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmdButton1.Location = new System.Drawing.Point(118, 114);
+            this.cmdButton1.Name = "cmdButton1";
+            this.cmdButton1.Size = new System.Drawing.Size(100, 100);
+            this.cmdButton1.TabIndex = 0;
+            this.cmdButton1.UseVisualStyleBackColor = false;
+            this.cmdButton1.Click += new System.EventHandler(this.cmdButton1_Click);
+            // 
+            // cmdButton2
+            // 
+            this.cmdButton2.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.cmdButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold);
+            this.cmdButton2.Location = new System.Drawing.Point(224, 114);
+            this.cmdButton2.Name = "cmdButton2";
+            this.cmdButton2.Size = new System.Drawing.Size(100, 100);
+            this.cmdButton2.TabIndex = 1;
+            this.cmdButton2.UseVisualStyleBackColor = false;
+            this.cmdButton2.Click += new System.EventHandler(this.cmdButton2_Click);
+            // 
+            // cmdButton4
+            // 
+            this.cmdButton4.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.cmdButton4.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold);
+            this.cmdButton4.Location = new System.Drawing.Point(118, 220);
+            this.cmdButton4.Name = "cmdButton4";
+            this.cmdButton4.Size = new System.Drawing.Size(100, 100);
+            this.cmdButton4.TabIndex = 3;
+            this.cmdButton4.UseVisualStyleBackColor = false;
+            this.cmdButton4.Click += new System.EventHandler(this.cmdButton4_Click);
+            // 
+            // cmdButton5
+            // 
+            this.cmdButton5.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.cmdButton5.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold);
+            this.cmdButton5.Location = new System.Drawing.Point(224, 220);
+            this.cmdButton5.Name = "cmdButton5";
+            this.cmdButton5.Size = new System.Drawing.Size(100, 100);
+            this.cmdButton5.TabIndex = 4;
+            this.cmdButton5.UseVisualStyleBackColor = false;
+            this.cmdButton5.Click += new System.EventHandler(this.cmdButton5_Click);
+            // 
+            // cmdButton6
+            // 
+            this.cmdButton6.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.cmdButton6.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold);
+            this.cmdButton6.Location = new System.Drawing.Point(330, 220);
+            this.cmdButton6.Name = "cmdButton6";
+            this.cmdButton6.Size = new System.Drawing.Size(100, 100);
+            this.cmdButton6.TabIndex = 5;
+            this.cmdButton6.UseVisualStyleBackColor = false;
+            this.cmdButton6.Click += new System.EventHandler(this.cmdButton6_Click);
+            // 
+            // cmdButton7
+            // 
+            this.cmdButton7.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.cmdButton7.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold);
+            this.cmdButton7.Location = new System.Drawing.Point(118, 326);
+            this.cmdButton7.Name = "cmdButton7";
+            this.cmdButton7.Size = new System.Drawing.Size(100, 100);
+            this.cmdButton7.TabIndex = 6;
+            this.cmdButton7.UseVisualStyleBackColor = false;
+            this.cmdButton7.Click += new System.EventHandler(this.cmdButton7_Click);
+            // 
+            // cmdButton8
+            // 
+            this.cmdButton8.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.cmdButton8.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold);
+            this.cmdButton8.Location = new System.Drawing.Point(224, 326);
+            this.cmdButton8.Name = "cmdButton8";
+            this.cmdButton8.Size = new System.Drawing.Size(100, 100);
+            this.cmdButton8.TabIndex = 7;
+            this.cmdButton8.UseVisualStyleBackColor = false;
+            this.cmdButton8.Click += new System.EventHandler(this.cmdButton8_Click);
+            // 
+            // cmdButton9
+            // 
+            this.cmdButton9.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.cmdButton9.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold);
+            this.cmdButton9.Location = new System.Drawing.Point(330, 326);
+            this.cmdButton9.Name = "cmdButton9";
+            this.cmdButton9.Size = new System.Drawing.Size(100, 100);
+            this.cmdButton9.TabIndex = 8;
+            this.cmdButton9.UseVisualStyleBackColor = false;
+            this.cmdButton9.Click += new System.EventHandler(this.cmdButton9_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            // 
+            // cmdNewgame
+            // 
+            this.cmdNewgame.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.cmdNewgame.Font = new System.Drawing.Font("Cooper Black", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmdNewgame.ForeColor = System.Drawing.Color.Black;
+            this.cmdNewgame.ImageAlign = System.Drawing.ContentAlignment.BottomLeft;
+            this.cmdNewgame.Location = new System.Drawing.Point(12, 460);
+            this.cmdNewgame.Name = "cmdNewgame";
+            this.cmdNewgame.Size = new System.Drawing.Size(174, 50);
+            this.cmdNewgame.TabIndex = 9;
+            this.cmdNewgame.Text = "Nouveau Jeu";
+            this.cmdNewgame.UseVisualStyleBackColor = true;
+            this.cmdNewgame.Click += new System.EventHandler(this.cmdNewgame_Click);
+            // 
+            // cmdQuit
+            // 
+            this.cmdQuit.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.cmdQuit.DialogResult = System.Windows.Forms.DialogResult.OK;
+            this.cmdQuit.Font = new System.Drawing.Font("Cooper Black", 15.75F);
+            this.cmdQuit.Location = new System.Drawing.Point(342, 460);
+            this.cmdQuit.Name = "cmdQuit";
+            this.cmdQuit.Size = new System.Drawing.Size(175, 50);
+            this.cmdQuit.TabIndex = 10;
+            this.cmdQuit.Text = "Quitter";
+            this.cmdQuit.UseVisualStyleBackColor = true;
+            this.cmdQuit.Click += new System.EventHandler(this.cmdQuit_Click);
+            // 
+            // cmdButton3
+            // 
+            this.cmdButton3.BackColor = System.Drawing.SystemColors.HighlightText;
+            this.cmdButton3.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold);
+            this.cmdButton3.Location = new System.Drawing.Point(330, 114);
+            this.cmdButton3.Name = "cmdButton3";
+            this.cmdButton3.Size = new System.Drawing.Size(100, 100);
+            this.cmdButton3.TabIndex = 2;
+            this.cmdButton3.UseVisualStyleBackColor = false;
+            this.cmdButton3.Click += new System.EventHandler(this.cmdButton3_Click);
+            // 
+            // cmdWin
+            // 
+            this.cmdWin.Image = ((System.Drawing.Image)(resources.GetObject("cmdWin.Image")));
+            this.cmdWin.Location = new System.Drawing.Point(-588, 100);
+            this.cmdWin.Name = "cmdWin";
+            this.cmdWin.Size = new System.Drawing.Size(500, 211);
+            this.cmdWin.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.cmdWin.TabIndex = 11;
+            this.cmdWin.TabStop = false;
+            this.cmdWin.Click += new System.EventHandler(this.cmdWin_Click);
+            // 
+            // affPlayer1
+            // 
+            this.affPlayer1.Image = ((System.Drawing.Image)(resources.GetObject("affPlayer1.Image")));
+            this.affPlayer1.Location = new System.Drawing.Point(-1009, 364);
+            this.affPlayer1.Name = "affPlayer1";
+            this.affPlayer1.Size = new System.Drawing.Size(670, 108);
+            this.affPlayer1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.affPlayer1.TabIndex = 12;
+            this.affPlayer1.TabStop = false;
+            this.affPlayer1.Click += new System.EventHandler(this.affPlayer1_Click);
+            // 
+            // affPlayer2
+            // 
+            this.affPlayer2.Image = ((System.Drawing.Image)(resources.GetObject("affPlayer2.Image")));
+            this.affPlayer2.Location = new System.Drawing.Point(-1131, 358);
+            this.affPlayer2.Name = "affPlayer2";
+            this.affPlayer2.Size = new System.Drawing.Size(670, 118);
+            this.affPlayer2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.affPlayer2.TabIndex = 13;
+            this.affPlayer2.TabStop = false;
+            // 
+            // frmAccueil
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("$this.BackgroundImage")));
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(529, 516);
+            this.Controls.Add(this.affPlayer2);
+            this.Controls.Add(this.affPlayer1);
+            this.Controls.Add(this.cmdWin);
+            this.Controls.Add(this.cmdQuit);
+            this.Controls.Add(this.cmdNewgame);
+            this.Controls.Add(this.cmdButton9);
+            this.Controls.Add(this.cmdButton8);
+            this.Controls.Add(this.cmdButton7);
+            this.Controls.Add(this.cmdButton6);
+            this.Controls.Add(this.cmdButton5);
+            this.Controls.Add(this.cmdButton4);
+            this.Controls.Add(this.cmdButton3);
+            this.Controls.Add(this.cmdButton2);
+            this.Controls.Add(this.cmdButton1);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(545, 555);
+            this.MinimumSize = new System.Drawing.Size(545, 555);
+            this.Name = "frmAccueil";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Morpion";
+            ((System.ComponentModel.ISupportInitialize)(this.cmdWin)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.affPlayer1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.affPlayer2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button cmdButton1;
+        private System.Windows.Forms.Button cmdButton2;
+        private System.Windows.Forms.Button cmdButton4;
+        private System.Windows.Forms.Button cmdButton5;
+        private System.Windows.Forms.Button cmdButton6;
+        private System.Windows.Forms.Button cmdButton7;
+        private System.Windows.Forms.Button cmdButton8;
+        private System.Windows.Forms.Button cmdButton9;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Button cmdNewgame;
+        private System.Windows.Forms.Button cmdQuit;
+        private System.Windows.Forms.Button cmdButton3;
+        private System.Windows.Forms.PictureBox cmdWin;
+        private System.Windows.Forms.PictureBox affPlayer1;
+        private System.Windows.Forms.PictureBox affPlayer2;
+    }
+}
+
---
+++ Morpion/frmAccueil.cs
@@ -0,0 +1,369 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Media;
+
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Morpion
+{
+    public partial class frmAccueil : Form
+    {
+
+        bool Joueur = true;
+        SoundPlayer winSound = new SoundPlayer(Properties.Resources.win);
+        
+        public frmAccueil()
+        {
+            InitializeComponent();   
+
+        }
+
+        private void cmdButton1_Click(object sender, EventArgs e)
+        {
+            
+            if (Joueur == true)
+            {
+                cmdButton1.Text = "X";
+                Joueur = false;
+            }
+            else 
+            { 
+                cmdButton1.Text = "O";
+                Joueur = true;
+            }
+
+            if (((cmdButton1.Text == cmdButton2.Text) & (cmdButton1.Text == cmdButton3.Text)) || 
+                ((cmdButton1.Text == cmdButton4.Text) & (cmdButton1.Text == cmdButton7.Text)) ||
+                ((cmdButton1.Text == cmdButton5.Text) & (cmdButton1.Text == cmdButton9.Text)))
+            {
+                cmdWin.Left += 600;
+                if(Joueur != true)
+                {
+                    winSound.Play();
+                    affPlayer1.Left += 1000;
+                }
+                else
+                {
+                    winSound.Play();
+                    affPlayer2.Left += 1000;
+                }
+                
+            }
+
+            cmdButton1.Enabled = false;
+        }
+
+        private void cmdButton2_Click(object sender, EventArgs e)
+        {
+            if (Joueur == true)
+            {
+                cmdButton2.Text = "X";
+                Joueur = false;
+            }
+            else
+            {
+                cmdButton2.Text = "O";
+                Joueur = true;
+            }
+
+            if (((cmdButton2.Text == cmdButton1.Text) & (cmdButton2.Text == cmdButton3.Text)) ||
+                ((cmdButton2.Text == cmdButton5.Text) & (cmdButton2.Text == cmdButton8.Text)))
+            {
+                cmdWin.Left += 600;
+                if (Joueur != true)
+                {
+                    winSound.Play();
+                    affPlayer1.Left += 1000;
+                }
+                else
+                {
+                    winSound.Play();
+                    affPlayer2.Left += 1000;
+                }
+            }
+
+            cmdButton2.Enabled = false;
+        }
+
+        private void cmdButton3_Click(object sender, EventArgs e)
+        {
+            if (Joueur == true)
+            {
+                cmdButton3.Text = "X";
+                Joueur = false;
+            }
+            else
+            {
+                cmdButton3.Text = "O";
+                Joueur = true;
+            }
+
+            if (((cmdButton3.Text == cmdButton1.Text) & (cmdButton3.Text == cmdButton2.Text)) ||
+                ((cmdButton3.Text == cmdButton5.Text) & (cmdButton3.Text == cmdButton7.Text)) ||
+                ((cmdButton3.Text == cmdButton6.Text) & (cmdButton3.Text == cmdButton9.Text)))
+            {
+                cmdWin.Left += 600;
+                if (Joueur != true)
+                {
+                    winSound.Play();
+                    affPlayer1.Left += 1000;
+                }
+                else
+                {
+                    winSound.Play();
+                    affPlayer2.Left += 1000;
+                }
+            }
+
+            cmdButton3.Enabled = false;
+        }
+
+        private void cmdButton4_Click(object sender, EventArgs e)
+        {
+            if (Joueur == true)
+            {
+                cmdButton4.Text = "X";
+                Joueur = false;
+            }
+            else
+            {
+                cmdButton4.Text = "O";
+                Joueur = true;
+            }
+
+            if (((cmdButton4.Text == cmdButton5.Text) & (cmdButton4.Text == cmdButton6.Text)) ||
+                ((cmdButton4.Text == cmdButton1.Text) & (cmdButton4.Text == cmdButton7.Text)))
+            {
+                cmdWin.Left += 600;
+                if (Joueur != true)
+                {
+                    winSound.Play();
+                    affPlayer1.Left += 1000;
+                }
+                else
+                {
+                    winSound.Play();
+                    affPlayer2.Left += 1000;
+                }
+            }
+            cmdButton4.Enabled = false;
+        }
+
+        private void cmdButton5_Click(object sender, EventArgs e)
+        {
+            if (Joueur == true)
+            {
+                cmdButton5.Text = "X";
+                Joueur = false;
+            }
+            else
+            {
+                cmdButton5.Text = "O";
+                Joueur = true;
+            }
+
+            if (((cmdButton5.Text == cmdButton1.Text) & (cmdButton5.Text == cmdButton9.Text)) ||
+                ((cmdButton5.Text == cmdButton2.Text) & (cmdButton5.Text == cmdButton8.Text)) ||
+                ((cmdButton5.Text == cmdButton3.Text) & (cmdButton5.Text == cmdButton7.Text)) ||
+                ((cmdButton5.Text == cmdButton4.Text) & (cmdButton5.Text == cmdButton6.Text)) 
+                )
+            {
+                cmdWin.Left += 600;
+                if (Joueur != true)
+                {
+                    winSound.Play();
+                    affPlayer1.Left += 1000;
+                }
+                else
+                {
+                    winSound.Play();
+                    affPlayer2.Left += 1000;
+                }
+            }
+
+            cmdButton5.Enabled = false;
+        }
+
+        private void cmdButton6_Click(object sender, EventArgs e)
+        {
+            if (Joueur == true)
+            {
+                cmdButton6.Text = "X";
+                Joueur = false;
+            }
+            else
+            {
+                cmdButton6.Text = "O";
+                Joueur = true;
+            }
+
+            if (((cmdButton6.Text == cmdButton3.Text) & (cmdButton6.Text == cmdButton9.Text)) ||
+                ((cmdButton6.Text == cmdButton5.Text) & (cmdButton6.Text == cmdButton4.Text)))
+            {
+                cmdWin.Left += 600;
+                if (Joueur != true)
+                {
+                    winSound.Play();
+                    affPlayer1.Left += 1000;
+                }
+                else
+                {
+                    winSound.Play();
+                    affPlayer2.Left += 1000;
+                }
+            }
+
+            cmdButton6.Enabled = false;
+        }
+
+        private void cmdButton7_Click(object sender, EventArgs e)
+        {
+            if (Joueur == true)
+            {
+                cmdButton7.Text = "X";
+                Joueur = false;
+            }
+            else
+            {
+                cmdButton7.Text = "O";
+                Joueur = true;
+            }
+
+            if (((cmdButton7.Text == cmdButton4.Text) & (cmdButton7.Text == cmdButton1.Text)) ||
+                ((cmdButton7.Text == cmdButton8.Text) & (cmdButton7.Text == cmdButton9.Text)) ||
+                ((cmdButton7.Text == cmdButton5.Text) & (cmdButton7.Text == cmdButton3.Text)))
+            {
+                cmdWin.Left += 600;
+                if (Joueur != true)
+                {
+                    winSound.Play();
+                    affPlayer1.Left += 1000;
+                }
+                else
+                {
+                    winSound.Play();
+                    affPlayer2.Left += 1000;
+                }
+            }
+
+            cmdButton7.Enabled = false;
+        }
+
+        private void cmdButton8_Click(object sender, EventArgs e)
+        {
+            if (Joueur == true)
+            {
+                cmdButton8.Text = "X";
+                Joueur = false;
+            }
+            else
+            {
+                cmdButton8.Text = "O";
+                Joueur = true;
+            }
+
+            if (((cmdButton8.Text == cmdButton7.Text) & (cmdButton8.Text == cmdButton9.Text)) ||
+                ((cmdButton8.Text == cmdButton5.Text) & (cmdButton8.Text == cmdButton2.Text)))
+            {
+                cmdWin.Left += 600;
+                if (Joueur != true)
+                {
+                    winSound.Play();
+                    affPlayer1.Left += 1000;
+                }
+                else
+                {
+                    affPlayer2.Left += 1000;
+                    winSound.Play();
+                }
+            }
+
+            cmdButton8.Enabled = false;
+        }
+
+        private void cmdButton9_Click(object sender, EventArgs e)
+        {
+            if (Joueur == true)
+            {
+                cmdButton9.Text = "X";
+                Joueur = false;
+            }
+            else
+            {
+                cmdButton9.Text = "O";
+                Joueur = true;
+            }
+
+            if (((cmdButton9.Text == cmdButton8.Text) & (cmdButton9.Text == cmdButton7.Text)) ||
+                ((cmdButton9.Text == cmdButton5.Text) & (cmdButton9.Text == cmdButton1.Text)) ||
+                ((cmdButton9.Text == cmdButton6.Text) & (cmdButton9.Text == cmdButton3.Text)))
+            {
+                cmdWin.Left += 600;
+                if (Joueur != true)
+                {
+                    winSound.Play();
+                    affPlayer1.Left += 1000;
+                }
+                else
+                {
+                    winSound.Play();
+                    affPlayer2.Left += 1000;
+                }
+            }
+
+            cmdButton9.Enabled = false;
+        }
+
+        private void cmdNewgame_Click(object sender, EventArgs e)
+        {
+            Joueur = true;
+            cmdWin.Left = -588;
+            cmdWin.Top = 100;
+            affPlayer1.Left = -1009;
+            affPlayer1.Top = 364;
+            affPlayer2.Left = -1131;
+            affPlayer2.Top = 358;
+            cmdButton1.Text = "";
+            cmdButton1.Enabled = true;
+            cmdButton2.Text = "";
+            cmdButton2.Enabled = true;
+            cmdButton3.Text = "";
+            cmdButton3.Enabled = true;
+            cmdButton4.Text = "";
+            cmdButton4.Enabled = true;
+            cmdButton5.Text = "";
+            cmdButton5.Enabled = true;
+            cmdButton6.Text = "";
+            cmdButton6.Enabled = true;
+            cmdButton7.Text = "";
+            cmdButton7.Enabled = true;
+            cmdButton8.Text = "";
+            cmdButton8.Enabled = true;
+            cmdButton9.Text = "";
+            cmdButton9.Enabled = true;
+        }
+
+        private void cmdQuit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void cmdWin_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void affPlayer1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+    }
+}
